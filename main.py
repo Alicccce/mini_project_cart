2c3a947425be7f0c1c2151d86c0c7bad5bc18a2d
1pyth2 <aliissa.mrak@mail.ru>
2024-02-06 20:03:21 +0300

    zd.2 разработка

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sys
 import os
 import requests
 
-map_request = f'http://static-maps.yandex.ru/1.x/?ll={int(input())}%2C{int(input())}&spn=3,5&l=map'
+x1, x2 = int(input()), int(input())
+map_request = f'http://static-maps.yandex.ru/1.x/?ll={x1}%2C{x2}&spn=3,5&l=map'
 response = requests.get(map_request)
 
 if not response:
@@ -17,11 +18,40 @@ with open(map_file, "wb") as file:
     file.write(response.content)
 
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+zoom = 2
 
+pr1, pr2 = False, False
+screen = pygame.display.set_mode((600, 450))
+mappp = pygame.image.load(map_file)
+
+
+mapr = mappp.get_rect()
+mapsurf = pygame.Surface((600, 450))
+mapsurf.blit(pygame.transform.scale(mappp,(600, 450)),(0,0))
+screen.blit(mapsurf,(0,0))
+scale = 1
+
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.K_PAGEDOWN:
+            zoom = 2
+            zoom_size = (int(600 / zoom), int(450 / zoom))
+            zoom_surf = .
+            screen.blit(zoom_surf, (0, 0))
+        if event.type == pygame.K_PAGEUP:
+            zoom = 0.5
+            zoom_size = (int(600 / zoom), int(450 / zoom))
+            zoom_surf = pygame.Surface(zoom_size)
+            zoom_surf.blit(screen, (0, 0), zoom_size)
+            zoom_surf = pygame.transform.smoothscale(zoom_surf, (600, 450))
+            screen.blit(zoom_surf, (0, 0))
+        pygame.display.flip()
+    #map_request = f'http://static-maps.yandex.ru/1.x/?ll={x1}%2C{x2}&spn={3/zoom},{5/zoom}&l=map'
+    #screen.blit(pygame.transform.scale(screen, (600, 450)), (0, 0))
+pygame.display.update()
+pygame.quit()
 os.remove(map_file)
